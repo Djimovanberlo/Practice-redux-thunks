ac2c7bdbabf47b75b2c3b55dad19c61683e0c251
Djimovanberlo <djimovanberlo@gmail.com>
2020-05-18 17:05:58 +0200

    setup, fetch and display (map, ternary) come data

---
+++ src/components/PostFeed.js
@@ -0,0 +1,48 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import moment from "moment";
+
+const API_URL = `https://codaisseur-coders-network.herokuapp.com`;
+
+export default function PostsFeed() {
+  const [data, setData] = useState({
+    loading: true,
+    posts: [],
+  });
+
+  async function fetchNext5Posts() {
+    setData({ ...data, loading: true });
+
+    const response = await axios.get(
+      `https://codaisseur-coders-network.herokuapp.com/posts?offset=0&limit=5`
+    );
+    const morePosts = response.data.rows;
+    console.log(response.data.rows);
+
+    setData({
+      loading: false,
+      posts: [...data.posts, ...morePosts],
+    });
+  }
+
+  useEffect(() => {
+    fetchNext5Posts();
+  }, []);
+
+  return (
+    <div className="PostsFeed">
+      <h2>Recent posts</h2>
+      <div>{data.loading ? <p>Loading...</p> : null}</div>
+      <div>
+        {data.posts.map((post) => {
+          return (
+            <div key={post.id}>
+              <h5>{post.title}</h5>
+              <p>{post.content}</p>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+}
---
+++ src/pages/HomePage.js
@@ -0,0 +1,11 @@
+import React from "react";
+import PostFeed from "../components/PostFeed";
+
+export default function HomePage() {
+  return (
+    <div>
+      <h1>Hello!</h1>
+      <PostFeed />
+    </div>
+  );
+}
